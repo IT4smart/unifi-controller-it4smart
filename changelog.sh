63371b9453cae485bff58e05bbf41162c0b72d03
hack3d <hack3d.dev@googlemail.com>
2019-12-12 07:53:58 +0100

    Add support for multiple distributions in changelog.

--- changelog.sh
+++ changelog.sh
@@ -3,6 +3,7 @@
 
 PKGNAME=$1
 DIST=$2
+DIST2=$3
 
 tmpdir=$(mktemp -d /tmp/git-tmp.XXXXXX) > /dev/null || exit 1
 
@@ -12,7 +13,7 @@ git clone https://github.com/IT4smart/unifi-controller-it4smart.git .
 git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
 git tag -l | sort -u -r | while read TAG ; do
 	if [ $NEXT ];then
-		echo "${PKGNAME} (${NEXT#'v'}~deb8) ${DIST}; urgency=low"
+		echo "${PKGNAME} (${NEXT#'v'}~${DIST2}) ${DIST}; urgency=low"
 	fi
 
 	GIT_PAGER=cat git log --no-merges --format="  * %s%n" $TAG..$NEXT
@@ -24,7 +25,7 @@ git tag -l | sort -u -r | while read TAG ; do
 done
 FIRST=$(git tag -l | head -1)
 echo
-echo "${PKGNAME} (${FIRST#'v'}~deb8) ${DIST}; urgency=low"
+echo "${PKGNAME} (${FIRST#'v'}~${DIST2}) ${DIST}; urgency=low"
 GIT_PAGER=cat git log --no-merges --format="  * %s%n" $FIRST
 echo " -- IT4smart  GmbH <support@it4smart.eu> $(git log -n1 --no-merges --format='%aD' $FIRST)"
 popd > /dev/null
